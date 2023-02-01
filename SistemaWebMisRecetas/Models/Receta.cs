5bad8381a1342105afcb82778307bca179234ca7
Franco Goyena <gfrancomariano@yahoo.com.ar>
2023-02-01 16:29:26 -0300

    agregue "." en los errormessage

--- SistemaWebMisRecetas/Models/Receta.cs
+++ SistemaWebMisRecetas/Models/Receta.cs
@@ -9,23 +9,23 @@ namespace SistemaWebMisRecetas.Models
     {
         public int Id { get; set; }
         public string Titulo { get; set; }
-        [Required(ErrorMessage = "La categoria es obligatoria")]
+        [Required(ErrorMessage = "La categoria es obligatoria.")]
         [CategoriadesayunoAttribute]
         public string Categoria { get; set; }
-        [Required(ErrorMessage = "Los ingredientes son obligatorios")]
+        [Required(ErrorMessage = "Los ingredientes son obligatorios.")]
         [DataType(DataType.MultilineText)]
         public string Ingredientes { get; set; }
-        [Required(ErrorMessage = "Las instrucciones son obligatorios")]
+        [Required(ErrorMessage = "Las instrucciones son obligatorios.")]
         [DataType(DataType.MultilineText)]
         public string Instrucciones { get; set; }
-        [Required(ErrorMessage = "El autor es obligatorio")]
+        [Required(ErrorMessage = "El autor es obligatorio.")]
         public string Autor { get; set; }
-        [Required(ErrorMessage = "El apellido es obligatorio")]
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
         public string Apellido { get; set; }
         [EdadMayorA18Attribute]
         public int Edad { get; set; }
-        [Required(ErrorMessage = "El email es obligatorio")]
-        [EmailAddressAttribute]
+        [Required(ErrorMessage = "El email es obligatorio.")]
+        [EmailAddressAttribute(ErrorMessage ="No es un email valido.")]
         public string Email { get; set; }
     }
 }
